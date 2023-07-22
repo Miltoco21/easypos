b5a3e59e447cd32a18e201af92cc0eaa7cdfc3c9
MiltonP <milton206@gmail.com>
2023-07-22 17:28:42 -0400

    export import excel  buttons

--- src/Componentes/Proveedores/IngresoPV.jsx
+++ src/Componentes/Proveedores/IngresoPV.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import axios from "axios";
-import  { useState } from "react";
+import { useState, useEffect } from "react";
 import InputAdornment from "@mui/material/InputAdornment";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
@@ -13,16 +14,13 @@ import Grid from "@mui/material/Grid";
 import { Tooltip } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Swal from "sweetalert2"
-import { saveAs } from 'file-saver';
-import * as xlsx from 'xlsx/xlsx.mjs';
-
-
-
-
+import Swal from "sweetalert2";
+import { saveAs } from "file-saver";
+import * as xlsx from "xlsx/xlsx.mjs";
+import withReactContent from "sweetalert2-react-content";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
 
 const defaultTheme = createTheme();
-
 
 const IngresoPV = () => {
   const [rut, setRut] = useState("");
@@ -33,42 +31,49 @@ const IngresoPV = () => {
   const [direccion, setDireccion] = useState("");
   const [comuna, setComuna] = useState("");
   const [sucursal, setSucursal] = useState("");
-  const [urlPagina, setUlrPagina] = useState("");
+  const [pagina, setUlrPagina] = useState("");
   const [formaPago, setFormaPago] = useState("");
   const [nombreResponsable, setNombreResponsable] = useState("");
   const [correoResponsable, setcorreoResponsable] = useState("");
   const [telefonoResponsable, setTelefonoResponsable] = useState("");
- 
   const [errors, setErrors] = useState({}); //error como objetos
+  const [showAlert, setShowAlert] = useState(false);
+  const [response, setResponse] = useState(null);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
+  const MySwal = withReactContent(Swal);
+
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
 
     reader.onload = (e) => {
       const data = new Uint8Array(e.target.result);
-      const workbook = xlsx.read(data, { type: 'array' });
-      const firstSheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[firstSheetName];
-      const jsonData = xlsx.utils.sheet_to_json(worksheet);
+      const workbook = xlsx.read(data, { type: "array" });
 
-      if (jsonData.length > 0) {
-        const firstDataRow = jsonData[0];
-        setRazonSocial(firstDataRow.razonSocial || "");
-        setGiro(firstDataRow.giro || "");
-        setEmail(firstDataRow.email || "");
-        setDireccion(firstDataRow.direccion || "");
-        setTelefono(firstDataRow.telefono || "");
-        setComuna(firstDataRow.comuna || "");
-        setSucursal(firstDataRow.sucursal || "");
-        setUlrPagina(firstDataRow.urlPagina || "");
-        setFormaPago(firstDataRow.formaPago || "");
-        setRut(firstDataRow.rut || "");
-        setNombreResponsable(firstDataRow.nombreResponsable || "");
-        setcorreoResponsable(firstDataRow.correoResponsable || "");
-        setTelefonoResponsable(firstDataRow.telefonoResponsable || "");
+      for (const sheetName of workbook.SheetNames) {
+        const worksheet = workbook.Sheets[sheetName];
+        const jsonData = xlsx.utils.sheet_to_json(worksheet);
+
+        // Archivo Excel debe tener headers = 'razonSocial', 'giro', 'email', etc.
+
+        if (jsonData.length > 0) {
+          const firstDataRow = jsonData[0];
+          setRazonSocial(firstDataRow.razonSocial || "");
+          setGiro(firstDataRow.giro || "");
+          setEmail(firstDataRow.email || "");
+          setDireccion(firstDataRow.direccion || "");
+          setTelefono(firstDataRow.telefono || "");
+          setComuna(firstDataRow.comuna || "");
+          setSucursal(firstDataRow.sucursal || "");
+          setUlrPagina(firstDataRow.pagina || "");
+          setFormaPago(firstDataRow.formaPago || "");
+          setRut(firstDataRow.rut || "");
+          setNombreResponsable(firstDataRow.nombreResponsable || "");
+          setcorreoResponsable(firstDataRow.correoResponsable || "");
+          setTelefonoResponsable(firstDataRow.telefonoResponsable || "");
+        }
       }
-
-     
     };
 
     reader.readAsArrayBuffer(file);
@@ -77,40 +82,56 @@ const IngresoPV = () => {
   const handleExportExcel = () => {
     const jsonData = [
       {
-        razonSocial:razonSocial,
-        giro:giro,
-        email:email,
-        direccion:direccion,
-        telefono:telefono,
-        comuna:comuna,
-        sucursal:sucursal,
-        urlPagina:urlPagina,
-        formaPago:formaPago,
-        rut:rut,
-        nombreResponsable:nombreResponsable,
-        correoResponsable:correoResponsable,
-        telefonoResponsable:telefonoResponsable,
-        
-      }
+        razonSocial: razonSocial,
+        giro: giro,
+        email: email,
+        direccion: direccion,
+        telefono: telefono,
+        comuna: comuna,
+        sucursal: sucursal,
+        pagina: pagina,
+        formaPago: formaPago,
+        rut: rut,
+        nombreResponsable: nombreResponsable,
+        correoResponsable: correoResponsable,
+        telefonoResponsable: telefonoResponsable,
+      },
     ];
 
     const worksheet = xlsx.utils.json_to_sheet(jsonData);
     const workbook = xlsx.utils.book_new();
-    xlsx.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    xlsx.utils.book_append_sheet(workbook, worksheet, "Sheet1");
     const excelBuffer = xlsx.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array',
+      bookType: "xlsx",
+      type: "array",
     });
-    const excelBlob = new Blob(
-      [excelBuffer],
-      {
-        type:
-          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-      }
-    );
-    saveAs(excelBlob, 'exported_data.xlsx');
+    const excelBlob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    saveAs(excelBlob, "exported_data.xlsx");
   };
-  
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
+  useEffect(() => {
+    if (showAlert && response) {
+      MySwal.fire({
+        title: <p>Guardado con éxito</p>,
+        icon: "success",
+        onClose: handleCloseAlert, // Close the alert when the user clicks outside or presses Esc
+      });
+    }
+  }, [showAlert, response]);
+
+  const displaySuccessAlert = () => {
+    MySwal.fire({
+      title: <p>Guardado con éxito</p>,
+      icon: "success",
+      onClose: handleCloseAlert,
+    });
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const errors = {};
@@ -132,9 +153,8 @@ const IngresoPV = () => {
     }
     if (!email) {
       errors.email = "Favor completar campo ";
-    }else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,8}$/.test(email)){
-      errors.email = "Formato de email no es válido"
-
+    } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,8}$/.test(email)) {
+      errors.email = "Formato de email no es válido";
     }
     if (!direccion) {
       errors.direccion = "Favor completar campo ";
@@ -148,8 +168,8 @@ const IngresoPV = () => {
     if (!giro) {
       errors.giro = "Favor completar campo ";
     }
-    if (!urlPagina) {
-      errors.urlPagina = "Favor completar campo ";
+    if (!pagina) {
+      errors.pagina = "Favor completar campo ";
     }
     if (!rut) {
       errors.rut = "Favor completar campo ";
@@ -169,7 +189,6 @@ const IngresoPV = () => {
     if (!telefonoResponsable) {
       errors.telefonoResponsable = "Favor completar campo ";
     }
-    
 
     if (Object.keys(errors).length > 0) {
       setErrors(errors);
@@ -182,13 +201,12 @@ const IngresoPV = () => {
         direccion,
         telefono,
         comuna,
-        urlPagina,
+        pagina,
         formaPago,
         rut,
         nombreResponsable,
         correoResponsable,
-        telefonoResponsable
-       
+        telefonoResponsable,
       };
       console.log(cliente);
 
@@ -197,63 +215,50 @@ const IngresoPV = () => {
           "https://www.easyposdev.somee.com/api/Proveedores/AddProveedor",
           cliente
         );
-        console.log(response.data.descripcion,'debugMiltoco')
+        setResponse(response.data);
+        setFormSubmitted(true);
+        console.log(response, "debugMiltoco");
 
-      
-       
-        Swal.fire({
-          icon: 'success',
-          title: 'Todo en orden',
-          text: (response.data.descripcion)
-          
-        })
+        setShowAlert(true);
 
-        setRazonSocial('');
-        setGiro('');
-        setEmail('');
-        setDireccion('');
-        setTelefono('');
-        setSucursal('');
-        setComuna('');
-        setUlrPagina('');
-        setFormaPago('');
-        setRut('');
-        setNombreResponsable('');
-        setcorreoResponsable('');
-        setTelefonoResponsable('');
-
-        
-
+        setRazonSocial("");
+        setGiro("");
+        setEmail("");
+        setDireccion("");
+        setTelefono("");
+        setSucursal("");
+        setComuna("");
+        setUlrPagina("");
+        setFormaPago("");
+        setRut("");
+        setNombreResponsable("");
+        setcorreoResponsable("");
+        setTelefonoResponsable("");
       } catch (error) {
         console.log(error.response.data, "Leer Error");
         Swal.fire({
-          position: 'top-end',
-          icon: 'error',
-          text:(error.response.data.descripcion),
-          title: (error.response.data.title),
-          
-          
-        })
-        
-        
+          position: "top-end",
+          icon: "error",
+          text: error.response.data.descripcion,
+          title: error.response.data.title,
+        });
       }
     }
   };
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: "100vh",width: "190vh" }}>
+      <Grid container component="main" sx={{ height: "100vh", width: "90vw" }}>
         <CssBaseline />
 
         <Grid
           item
-          xs={1}
+          xs={12}
           sm={8}
           md={14}
           component={Paper}
           elevation={24}
           square
         >
-          
           <Box
             sx={{
               my: 1,
@@ -268,7 +273,6 @@ const IngresoPV = () => {
               component="form"
               noValidate
               onSubmit={handleSubmit}
-              
               sx={{ mt: 2 }}
             >
               <Grid container spacing={2}>
@@ -342,9 +346,7 @@ const IngresoPV = () => {
                       endAdornment: (
                         <InputAdornment position="end">
                           {email &&
-                          /^[\w-.]+@([\w-]+\.)+[\w-]{2,8}$/.test(
-                            email
-                          ) ? (
+                          /^[\w-.]+@([\w-]+\.)+[\w-]{2,8}$/.test(email) ? (
                             <Tooltip title="Correct rut format" placement="top">
                               <CheckCircleIcon style={{ color: "green" }} />
                             </Tooltip>
@@ -355,7 +357,7 @@ const IngresoPV = () => {
                   />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                  <TextField  
+                  <TextField
                     error={!!errors.telefono}
                     helperText={errors.telefono}
                     required
@@ -412,15 +414,15 @@ const IngresoPV = () => {
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                   <TextField
-                    error={!!errors.urlPagina}
-                    helperText={errors.urlPagina }
+                    error={!!errors.pagina}
+                    helperText={errors.pagina}
                     required
                     fullWidth
-                    name="urlPagina"
-                    label="urlPagina"
+                    name="pagina"
+                    label="Página"
                     type="text"
-                    id="urlPagina"
-                    value={urlPagina}
+                    id="pagina"
+                    value={pagina}
                     onChange={(e) => setUlrPagina(e.target.value)}
                   />
                 </Grid>
@@ -480,34 +482,53 @@ const IngresoPV = () => {
                     onChange={(e) => setTelefonoResponsable(e.target.value)}
                   />
                 </Grid>
-                
-                
-                
-
-                
-                
               </Grid>
-
               <Button
                 type="submit"
                 size="md"
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                guardar 
-              </Button>
-              <input type="file" onChange={handleFileUpload} />
-              <button onClick={handleExportExcel}>Export to Excel</button>
-
-              
-             
+                guardar
+              </Button>{" "}
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6} md={5}>
+                  <Button
+                    fullWidth
+                    variant="outlined"
+                    size="md"
+                    onClick={handleExportExcel}
+                  >
+                    Exportar a Excel
+                  </Button>
+                </Grid>
+                <Grid item xs={12} sm={6} md={5}>
+                  <Box sx={{ display: "flex",  }}>
+                    <TextField
+                      fullWidth
+                      variant="outlined"
+                     
+                      size="small"
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <UploadFileIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      type="file"
+                      onChange={handleFileUpload}
+                    />
+                  </Box>
+                </Grid>
+              </Grid>
             </Box>
           </Box>
         </Grid>
       </Grid>
+      {formSubmitted && response && displaySuccessAlert()}
     </ThemeProvider>
-    
-  )
-}
+  );
+};
 
-export default IngresoPV
+export default IngresoPV;
