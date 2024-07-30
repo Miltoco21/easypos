b43b9068f6013058b8527adf713758f0a2fa964c
MiltonP <milton206@gmail.com>
2024-07-30 17:04:30 -0400

    ajustes nav reportes

--- src/Pages/Reportes.jsx
+++ src/Pages/Reportes.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import {
   Grid,
-  Card,
-  CardContent,
-  CardActions,
-  Button,
   Typography,
   Table,
   TableBody,
@@ -13,6 +9,7 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Button,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import SideBar from "../Componentes/NavBar/SideBar";
@@ -24,50 +21,26 @@ const Reportes = () => {
     {
       title: "Cuentas Corrientes Clientes",
       path: "/reportes/cuentacorrienteclientes",
-      data: [
-        { name: "Cliente 1", amount: "$1000" },
-        { name: "Cliente 2", amount: "$2000" },
-      ],
     },
     {
       title: "Cuentas Corrientes Proveedores",
       path: "/reportes/cuentacorrienteproveedores",
-      data: [
-        { name: "Proveedor 1", amount: "$1500" },
-        { name: "Proveedor 2", amount: "$2500" },
-      ],
     },
     {
       title: "Ranking de Venta",
       path: "/reportes/rankingventas",
-      data: [
-        { name: "Venta 1", amount: "$3000" },
-        { name: "Venta 2", amount: "$4000" },
-      ],
     },
     {
       title: "Ranking de Venta de Productos",
       path: "/reportes/rankingproductos",
-      data: [
-        { name: "Producto 1", amount: "$500" },
-        { name: "Producto 2", amount: "$600" },
-      ],
     },
     {
       title: "Libro Ventas",
       path: "/reportes/rankinglibroventas",
-      data: [
-        { name: "Libro 1", amount: "$700" },
-        { name: "Libro 2", amount: "$800" },
-      ],
     },
     {
       title: "Libro Compras",
       path: "/reportes/rankinglibrocompras",
-      data: [
-        { name: "Compra 1", amount: "$900" },
-        { name: "Compra 2", amount: "$1000" },
-      ],
     },
   ];
 
@@ -75,41 +48,48 @@ const Reportes = () => {
     <div style={{ display: "flex" }}>
       <SideBar />
       <Grid component="main" sx={{ flexGrow: 1, p: 2 }}>
-        <Typography variant="h4" component="div" sx={{ mb: 2 }}>
+        <Typography
+          variant="h4"
+          component="div"
+          sx={{ mb: 4, textAlign: "center" }}
+        >
           Reportes
         </Typography>
-        <Grid container alignItems="center" spacing={1} sx={{ flexGrow: 1, p: 2 }}>
+        <Grid container spacing={3} justifyContent="center">
           {reportCards.map((report, index) => (
-            <Grid item xs={12} sm={6} md={6} key={index}>
-              <Card sx={{ margin: 2, height: "auto" }}>
-                <CardContent>
-                  {/* <Typography variant="h5" component="div" sx={{ backgroundColor: "gainsboro" }}>
-                    {report.title}
-                  </Typography> */}
-                  <TableContainer component={Paper} sx={{ mt: 2 }}>
-                    <Table size="small">
-                      <TableHead>
-                        <TableRow sx={{ mt: 2 }}>
-                          <TableCell fullWidth > {report.title}</TableCell>
-                          {/* <TableCell align="right">Amount</TableCell> */}
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                      <Button
-                      fullWidth
-                    variant="contained"
-                    color="secondary"
-                    size="large"
-                    onClick={() => navigate(report.path)}
-                  >
-                    Ver reportes
-                  </Button>
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                </CardContent>
-               
-              </Card>
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <TableContainer component={Paper} sx={{ p: 2 }}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell
+                        align="center"
+                        sx={{
+                          fontWeight: "bold",
+                          backgroundColor: "gainsboro",
+                        }}
+                      >
+                        {report.title}
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center">
+                        <Button
+                          variant="contained"
+                          fullWidth
+                          size="large"
+                          onClick={() => navigate(report.path)}
+                          sx={{ mt: 2 }}
+                        >
+                          Ir a reportes
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </TableContainer>
             </Grid>
           ))}
         </Grid>
